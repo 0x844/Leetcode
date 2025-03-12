43388ad8cf514722405cfadc21d7f84e0f32d527
0x844 <139642415+0x844@users.noreply.github.com>
2025-03-12 17:32:16 -0400

    169. Majority Element

---
+++ 169MajorityElement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = {}
+
+        for num in nums:
+            if num not in hashmap:
+                hashmap[num] = 1
+            else:
+                hashmap[num] += 1
+        for key in hashmap.keys():
+            if hashmap[key] > len(nums) // 2:
+                return key
